66fcef2c429aded1285c242682991e921f973c91
shofwanalif <shofwanaliffigma18@gmail.com>
2025-01-24 21:50:32 +0700

    update 2

--- script.js
+++ script.js
@@ -8,13 +8,12 @@ const spentModalForm = document.getElementById('spentmodal_form');
 const closeModal = document.querySelector('#modal_form .modal_heading i');
 const addExpendBtn = document.querySelector('#budget_details button.add_expend');
 const closeExpend = document.querySelector('#spentmodal_form .modal_heading i');
+const notification = document.getElementById('notification');
 
 backHomeBtn.addEventListener('click', () => {
   budgetDetails.classList.add('hidden');
   budgetsPage.classList.remove('hidden');
 });
-
-console.log(cardBudgets);
 
 cardBudgets.forEach((card) => {
   card.addEventListener('click', () => {
@@ -28,8 +27,12 @@ addBudgetBtn.addEventListener('click', () => {
 });
 
 closeModal.addEventListener('click', () => {
-  modalForm.classList.add('hidden');
+  closeModalBudget();
 });
+
+function closeModalBudget() {
+  modalForm.classList.add('hidden');
+}
 
 addExpendBtn.addEventListener('click', () => {
   spentModalForm.classList.remove('hidden');
@@ -37,4 +40,51 @@ addExpendBtn.addEventListener('click', () => {
 
 closeExpend.addEventListener('click', () => {
   spentModalForm.classList.add('hidden');
+});
+
+function getFormValue(formData) {
+  let result = new Object();
+
+  for (const data of formData.entries()) {
+    const [key, value] = data;
+    Object.assign(result, {
+      [key]: value,
+    });
+  }
+
+  return result;
+}
+
+function saveData(databaru) {
+  const existingData = JSON.parse(localStorage.getItem('Budgets')) ?? [];
+  existingData.push(databaru);
+  localStorage.setItem('Budgets', JSON.stringify(existingData));
+}
+
+function resetInput() {
+  document.querySelectorAll('form input').forEach((input) => {
+    input.value = '';
+  });
+}
+
+function showNotif(message) {
+  const newNotification = document.createElement('div');
+  newNotification.innerText = message;
+  newNotification.classList.add('notification');
+
+  notification.appendChild(newNotification);
+
+  setTimeout(() => {
+    notification.removeChild(newNotification);
+  }, 1800);
+}
+
+// submit form
+document.querySelector('#modal_form form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = getFormValue(new FormData(e.target));
+  saveData(data);
+  closeModalBudget();
+  resetInput();
+  showNotif('✅ Budget berhasil di catat!');
 });
